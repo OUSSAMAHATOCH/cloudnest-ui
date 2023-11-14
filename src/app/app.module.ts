25c5f40cbc8f9e3e13bd5aa6a79c3916d7141f57
OUSSAMAHATOCH <oussama.hatoch200@gmail.com>
2023-11-14 20:52:43 +0100

    create cloudnest-ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { SigninComponent } from './signin/signin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    SigninComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
